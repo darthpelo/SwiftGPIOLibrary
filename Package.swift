d8d6e72187bff2b7cb85bb2d95ee26bfd974dd6e
Alessio Roberto <darthpelo@gmail.com>
2017-04-09 19:44:20 +0200

    ✨ Update Package.swift to use SwiftyGPIO 0.8 to support Swift 3.0

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftGPIOLibrary",
     dependencies: [
-        .Package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 0),
+        .Package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 0, minor: 8)
     ]
 )
